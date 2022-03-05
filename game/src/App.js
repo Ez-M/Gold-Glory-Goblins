7c3f8d8037b1d0ff0ac6b2516413f0c4ac8c7265
Ezequiel <martinez.ezequiel42@gmail.com>
2022-03-04 16:42:50 -0800

    created gold, score, and currentview variables in app.js Added button that displays gold value

--- game/src/App.js
+++ game/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css"; 
+import "bootstrap/dist/css/bootstrap.min.css"; 
 
 function App() {
+
+  var gold = 0;
+  var score = 0;
+  var currentView = "";
+
   return (<div className="App">
     Hello World!
+    <button>{gold}</button>
   </div>
   );
 }
