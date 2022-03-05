f172f2a5a19d5f1b25a615390e7f725508f69b72
Ezequiel <martinez.ezequiel42@gmail.com>
2022-03-04 16:31:52 -0800

    tested hello world render and created components folder with grind and spend.js

--- game/src/App.js
+++ game/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"; 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css"; 
 
 function App() {
-  return ( placeholder);
+  return (<div className="App">
+    Hello World!
+  </div>
+  );
 }
 
 export default App;
