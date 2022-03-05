68f3158bffb22a9daa37adcd25b7e0d9dcbb9301
Ezequiel <martinez.ezequiel42@gmail.com>
2022-03-04 20:15:00 -0800

    spend button now actually costs gold to use

--- game/src/components/spend.js
+++ game/src/components/spend.js
@@ -5,8 +5,13 @@ import { GSCV } from "../contexts/GSCV";
 function Spend() {
 
     const {gold, score, currentView, damage, setGold, setScore,setCurrentView, setDamage} = useContext(GSCV) //importing context from GSCV
+   //BELOW: a function for increasing the damage value in exchange for gold
     function damageUp(){
+        if (gold >= damage*5) {
+        setGold(gold - damage*5)
         setDamage(damage+1);
+
+        }
     }
 
 
