4227cae797d65ffb9802b2461ef32ea2df3ceb6f
Ezequiel <martinez.ezequiel42@gmail.com>
2022-03-04 18:05:18 -0800

    converted original code to use states in order to render variables dynamically

--- game/src/App.js
+++ game/src/App.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; 
 
 function App() {
 
-  var gold = 45; //Player's current gold
+  var gold = 0; //Player's current gold
   var score = 0; //Player's total gold ever earned (plus achivements?)
   var currentView = ""; //variable to tell the game what tab to render
 
-  return (<div className="App">
+
+  //This is me doing the same thing as above but using states because I think I need to
+  // in order to get things to dynamically render/update
+const [gold2, setGold2] = useState(0);
+const [score2, setScore2] = useState(0);
+const [currentView2, setCurrentView2] = useState("");
+
+
+  //testClick function to increase gold and score when clicked, simulating
+  //resource gain during gameplay
+  // PROBLEM: currently not updating the onscreen render
+  function testClick(){
+    setGold2(gold2 + 1)
+    setScore2(score2 + 1)
+    console.log(`gold: ${gold2}`)
+    console.log(`score: ${score2}`)
+  }
+
+  return (
+  <div className="App">
     Hello Adventurer!
-    <button>{gold}</button>
     <br/>
-    <p>gold: {gold}  score: {score} currentView: {currentView}</p>
+    Punch that goblin!
+    <button onClick={testClick}>{gold2}</button>
+    <br/>
+    <p>gold: {gold2}  score: {score2} currentView: {currentView2}</p> 
   </div>
   );
 }
